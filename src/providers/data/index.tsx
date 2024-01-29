8dafbbc80cd0a494cf9bf08d2144d7e11b1ffcfd
Rohit Gaikwad <Rohitgaikwad78963@gmail.com>
2024-01-29 22:52:26 +0530

    auth provider

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -6,10 +6,10 @@ import { createClient } from "graphql-ws";
 import { fetchWrapper } from "./fetch-wrapper";
 
 export const API_BASE_URL = "https://api.crm.refine.dev";
-export const API_URl = "https://api.crm.refine.dev";
+export const API_URL = "https://api.crm.refine.dev";
 export const WS_URL = "wss://api.crm.refine.dev/graphql";
 
-export const client = new GraphQLClient(API_URl, {
+export const client = new GraphQLClient(API_URL, {
   fetch: (url: string, options: RequestInit) => {
     try {
       return fetchWrapper(url, options);
